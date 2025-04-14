ea2f9bc4ccdcde9e2315ac1bf8a36becc47312a1
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-04-14 10:40:48 -0400

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # noiz
+
+A simple noise library built for and with Bevy.
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![no_std]
+#![allow(
+    clippy::doc_markdown,
+    reason = "These rules should not apply to the readme."
+)]
+#![doc = include_str!("../README.md")]
+
+#[cfg(test)]
+extern crate alloc;
